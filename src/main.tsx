07c1a7b6694d4bf35529569a76ce15ea7028b231
Art <absolutus11@gmail.com>
2024-04-04 21:20:53 +0400

    fix: imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
-import store from "./app/store.tsx";
-import App from './app/App.tsx'
+import store from "./app/store";
+import App from './app/App'
 
 import "./components/styles/main.css";
 
